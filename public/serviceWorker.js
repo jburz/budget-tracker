1ea946ceb9a0808ad16e3a5339c81b8351ee65c7
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-11-25 21:23:06 -0700

    updated install event listener to add files to cache

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,9 +1,28 @@
 console.log("hit serviceWorker.js");
 
+const cache = 'budget-tracker'
+const cacheFiles = [
+    '/',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'index.html',
+    'index.js',
+    'manifest.webmanifest',
+    'serviceWorker.js',
+    'styles.css'
+];
 
 //service worker update
 self.addEventListener('install', event => {
     console.log("hit install");
+    event.waitUntil(
+        caches
+            .open(cache)
+            .then(cache => {
+                return cache.addAll(cacheFiles);
+            })
+            .catch(err => console.log('Error caching files on install: ', err))
+    );
 
 });
 
