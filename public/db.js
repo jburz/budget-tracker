07df632201eddd9e36e4373941282b3bfca758fe
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-11-30 19:56:51 -0700

    indexeddb functionality

---
+++ public/db.js
@@ -0,0 +1,65 @@
+//create new database
+let db;
+//db name and version #
+const request = indexedDB.open("budget", 1);
+
+//create object store on upgrade
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+//successful database connection
+request.onsuccess = (event) => {
+    db = event.targt.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+//database connection error
+request.onerror = (event) => {
+    console("Error connecting to database: ", event.target.errorCode);
+}
+
+//function to save a transaction record to the database
+function saveRecord(record) {
+    //create transaction with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    //access objectStore
+    const store = transaction.objectStore("pending");
+
+    //add record
+    store.add(record);
+}
+
+//function to check database transactions
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    //get all records
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*", "Content-Type": "application/json"
+                }
+            }).then(res => res.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.store("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+
+//check if online
+window.addEventListener("online", checkDatabase);
